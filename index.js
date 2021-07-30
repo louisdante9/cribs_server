2a7839d9a31f64f9539fd77bd59780f66694820d
louis nwamadi <louisdante9@gmail.com>
2021-07-30 12:16:23 +0100

    add apartment and user controller

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ import express from 'express';
 import cors from 'cors';
 import { json, urlencoded } from 'body-parser';
 
-import { logger } from './utils'
+import { logger } from './utils';
 import config from './config';
 import db from './config/db';
 // const routes = require('./routes');
 import { GlobalErrorHandler } from './middlewares';
 
-console.log(config, 'hello there')
+console.log(config, 'hello there');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors())
+app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: false }));
 
@@ -21,17 +21,19 @@ db(config)
   .then(() => {
     // app.use('/v1', routes(express));
     app.use(GlobalErrorHandler);
-    app.get('/', (req, res)=> {
-        res.send('hello there')
-    })
+    app.get('/', (req, res) => {
+      res.send('hello there');
+    });
 
-    app.listen(port, err => {
+    app.listen(port, (err) => {
       if (err) {
         throw new Error(err.message);
       }
       logger.info(`rezerv service is running on port ${port}`);
     });
   })
-  .catch(err => { throw new Error(`Connection error: ${err.message}`); });
+  .catch((err) => {
+    throw new Error(`Connection error: ${err.message}`);
+  });
 
 module.exports = app;
